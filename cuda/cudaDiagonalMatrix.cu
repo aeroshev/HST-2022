45756d7ad56dbbfeb496c6fbd4cc30fba44ef133
Artem Eroshev <aeroshev@noreply>
2022-11-07 05:39:36 -0500

    Add CUDA_CHECK

--- cuda/cudaDiagonalMatrix.cu
+++ cuda/cudaDiagonalMatrix.cu
@@ -1,6 +1,28 @@
-#include <stdio.h>
+// C++ Libs
+#include <iostream>
+// CUDA libs
 #include <cuda_runtime.h>
 #include <helper_cuda.h>
+
+using namespace std;
+
+
+#define CUDA_CHECK(call)                                                        \
+    if ((call) != cudaSuccess) {                                                \
+        cudaError_t err = cudaGetLastError();                                   \
+        cerr << "CUDA error calling \"" #call "\", code is " << err << "\n";    \
+        exit(1);                                                                \
+    }
+
+
+void printMatrix(float *matrix, size_t N) {
+    for (size_t i = 0; i < N; i++) {
+        for (size_t j = 0; j < N; j++) {
+            cout << matrix[i * N + j] << ' ';
+        }
+        cout << '\n';
+    }
+}
 
 
 __global__ void matrixDiagonal(float *A, float *sum, int N) {
@@ -10,6 +32,7 @@ __global__ void matrixDiagonal(float *A, float *sum, int N) {
     if (row < column) {
         *sum += A[row * N + column];
     }
+
  }
 
 
@@ -21,37 +44,37 @@ int main(int argc, char *argv[]) {
     // Allocate matrix on CPU
     float *h_matrix_A = (float *)malloc(matrixSize);
     for (size_t i = 0; i < N * N; i ++) {
-        h_matrix_A[i] = rand() / (float)RAND_MAX;
+        // h_matrix_A[i] = rand() / (float)RAND_MAX;
+        h_matrix_A[i] = 1;  // debug
     }
+    printMatrix(h_matrix_A, N);
 
     float *h_sum = (float *)calloc(1, floatSize);
 
-    cudaError_t err = cudaSuccess;
     // Allocate memory to GPU Device
     float *d_matrix_A = NULL;
-    err = cudaMalloc((void **)&d_matrix_A, matrixSize);
+    CUDA_CHECK(cudaMalloc((void **)&d_matrix_A, matrixSize));
 
     float *d_sum = NULL;
-    err = cudaMalloc((void **)&d_sum, floatSize);
+    CUDA_CHECK(cudaMalloc((void **)&d_sum, floatSize));
 
     // Copy data from CPU to GPU
-    err = cudaMemcpy(d_matrix_A, h_matrix_A, matrixSize, cudaMemcpyHostToDevice);
-    err = cudaMemcpy(d_sum, h_sum, floatSize, cudaMemcpyHostToDevice);
+    CUDA_CHECK(cudaMemcpy(d_matrix_A, h_matrix_A, matrixSize, cudaMemcpyHostToDevice));
+    CUDA_CHECK(cudaMemcpy(d_sum, h_sum, floatSize, cudaMemcpyHostToDevice));
 
     // Launch kernel
-    dim3 threadsPerBlock(16, 16);
+    dim3 threadsPerBlock(N / 2, N / 2);
     dim3 numBlocks(N / threadsPerBlock.x, N / threadsPerBlock.y);
-    matrixDiagonal<<<1, threadsPerBlock>>>(d_matrix_A, d_sum, N);
-    err = cudaGetLastError();
+    matrixDiagonal<<<numBlocks, threadsPerBlock>>>(d_matrix_A, d_sum, N);
 
     // Copy result from GPU to CPU
-    err = cudaMemcpy(h_sum, d_sum, floatSize, cudaMemcpyDeviceToHost);
+    CUDA_CHECK(cudaMemcpy(h_sum, d_sum, floatSize, cudaMemcpyDeviceToHost));
 
-    printf("Result of sum: %d\n", *h_sum);
+    cout << "Result of sum:" << *h_sum << "\n";
 
     // Free memory
-    err = cudaFree(d_matrix_A);
-    err = cudaFree(d_sum);
+    CUDA_CHECK(cudaFree(d_matrix_A));
+    CUDA_CHECK(cudaFree(d_sum));
 
     free(h_matrix_A);
     free(h_sum);
